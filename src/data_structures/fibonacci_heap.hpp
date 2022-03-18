be418977c9b96446e5c683d493133551d5fe7e6c
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-03-18 18:31:53 +0000

    Add data structure files

---
+++ src/data_structures/fibonacci_heap.hpp
@@ -0,0 +1,9 @@
+
+#ifndef FIBONACCI_HEAP_H
+#define FIBONACCI_HEAP_H
+
+class FibonacciHeap {
+    // TODO
+};
+
+#endif // FIBONACCI_HEAP_H
---
+++ src/data_structures/quadtree.hpp
@@ -0,0 +1,9 @@
+
+#ifndef QUADTREE_H
+#define QUADTREE_H
+
+class Quadtree {
+    // TODO
+};
+
+#endif // QUADTREE_H
