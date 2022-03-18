06dcd4a57efaa1c9f95233ed02f8cbfae0f4f1ca
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-03-18 21:26:58 +0000

    Add AStar algorithm file

---
+++ src/algorithms/a_star.hpp
@@ -0,0 +1,13 @@
+
+#ifndef A_STAR_H
+#define A_STAR_H
+
+#include <utility>
+#include <list>
+#include "types.hpp"
+#include "graph.hpp"
+
+template <typename T>
+std::pair<std::list<u64>, double> aStarSearch(Graph<T> g, u64 start, u64 end);
+
+#endif // A_STAR_H
