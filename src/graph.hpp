b6a0d5fc73045442128b1ba415c6ab0be86ce307
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-03-18 15:36:08 +0000

    Implement basic graph

--- src/graph.hpp
+++ src/graph.hpp
@@ -2,25 +2,36 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <vector>
+#include <list>
 #include <unordered_map>
-
-template <typename T>
-class Vertex {
-    public:
-        const T& getData() const;
-        const std::unordered_map<Vertex<T>*, double> getAdjacent() const;
-    private:
-        T data;
-        std::unordered_map<Vertex<T>*, double> adjacent;
-};
+#include <string>
+#include "types.hpp"
 
 template <typename T>
 class Graph {
     public:
-        const std::vector<Vertex<T>>& getVertices() const;
+        Graph() : adjList({}) {}
+        void addNode(u64 id, T data) {
+            nodes[id] = data;
+            adjList[id] = {};
+        }
+
+        void addEdge(u64 node1, u64 node2, double weight) {
+            if (adjList.count(node1) != 0 && adjList.count(node2) != 0) {
+                adjList[node1].push_back(std::make_pair(node2, weight));
+            }
+        }
+
+        const T& getNode(u64 id) {
+            return nodes.at(id);
+        }
+
+        const std::list<std::pair<u64, double>>& getEdges(u64 id) const {
+            return adjList.at(id);
+        }
     private:
-        std::vector<Vertex<T>> vertices;
+        std::unordered_map<u64, T> nodes;
+        std::unordered_map<u64, std::list<std::pair<u64, double>>> adjList;
 };
 
 #endif // GRAPH_H
---
+++ src/types.hpp
@@ -0,0 +1,12 @@
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <cstdint>
+
+typedef int32_t i32;
+typedef int64_t i64;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif // TYPES_H
