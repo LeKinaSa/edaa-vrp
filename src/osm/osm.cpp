0554d3202a0b444cfab85a1c6c41a80282325335
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2022-03-31 22:32:59 +0100

    Improve OSM parser tag handling

--- src/osm/osm.cpp
+++ src/osm/osm.cpp
@@ -1,6 +1,7 @@
 
 #include <string>
 #include <iostream>
+#include <unordered_set>
 #include <tinyxml/tinyxml2.h>
 #include "osm.hpp"
 
@@ -38,41 +39,53 @@ OsmXmlData parseOsmXml(const char* path) {
         nodeElement = nodeElement->NextSiblingElement("node");
     }
 
+    static const unordered_set<string> invalidHighwayValues = {
+        "service", "pedestrian", "track", "escape", "raceway", "busway",
+        "footway", "bridleway", "steps", "corridor", "path", "cycleway",
+        "proposed", "construction", "elevator"
+    };
+
     const XMLElement* way = root->FirstChildElement("way");
     while (way != nullptr) {
         bool oneWay = true;
+        bool valid = true;
 
         const XMLElement* tag = way->FirstChildElement("tag");
         while (tag != nullptr) {
-            if (tag->Attribute("k", "oneway")) {
-                if (tag->Attribute("v", "no")) {
-                    oneWay = false;
-                }
-                break;
+            if (tag->Attribute("k", "oneway") && tag->Attribute("v", "no")) {
+                oneWay = false;
+            }
+
+            if ((tag->Attribute("k", "access") && tag->Attribute("v", "no")) ||
+                (tag->Attribute("k", "highway") && invalidHighwayValues.count(tag->Attribute("v")) != 0)) {
+                valid = false;
             }
 
             tag = tag->NextSiblingElement("tag");
         }
 
-        const XMLElement* n1 = way->FirstChildElement("nd");
-        const XMLElement* n2 = n1->NextSiblingElement("nd");
+        if (valid) {
+            const XMLElement* n1 = way->FirstChildElement("nd");
+            const XMLElement* n2 = n1->NextSiblingElement("nd");
 
-        while (n2 != nullptr) {
-            u64 id1 = stoull(n1->Attribute("ref")), 
-                id2 = stoull(n2->Attribute("ref"));
+            while (n2 != nullptr) {
+                u64 id1 = stoull(n1->Attribute("ref")), 
+                    id2 = stoull(n2->Attribute("ref"));
 
-            const OsmNode& data1 = data.graph.getNode(id1),
-                data2 = data.graph.getNode(id2);
+                const OsmNode& data1 = data.graph.getNode(id1),
+                    data2 = data.graph.getNode(id2);
 
-            double weight = data1.coordinates.haversine(data2.coordinates);
-            data.graph.addEdge(id1, id2, weight);
-            if (!oneWay) {
-                data.graph.addEdge(id2, id1, weight);
+                double weight = data1.coordinates.haversine(data2.coordinates);
+                data.graph.addEdge(id1, id2, weight);
+                if (!oneWay) {
+                    data.graph.addEdge(id2, id1, weight);
+                }
+
+                n1 = n2;
+                n2 = n2->NextSiblingElement("nd");
             }
-
-            n1 = n2;
-            n2 = n2->NextSiblingElement("nd");
         }
+
 
         way = way->NextSiblingElement("way");
     }
