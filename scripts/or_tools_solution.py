11cd8b175a4f9930fff65115cc79ad41c4028aad
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2022-05-12 18:17:30 +0100

    Complete redesign of the model

--- scripts/or_tools_solution.py
+++ scripts/or_tools_solution.py
@@ -1,124 +1,122 @@
-from ortools.sat.python import cp_model
+# Code adapted from https://developers.google.com/optimization/routing/cvrp
+
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
 
 def create_data_model():
     """Stores the data for the problem."""
     data = {}
     data['distance_matrix'] = [
-        [0,   548,  776,  696,  582,  274,  502,  194, 308,  194, 536,  502,  388,  354,  468,  776,  662 ],
-        [548, 0,    684,  308,  194,  502,  730,  354, 696,  742, 1084, 594,  480,  674,  1016, 868,  1210],
-        [776, 684,  0,    992,  878,  502,  274,  810, 468,  742, 400,  1278, 1164, 1130, 788,  1552, 754 ],
-        [696, 308,  992,  0,    114,  650,  878,  502, 844,  890, 1232, 514,  628,  822,  1164, 560,  1358],
-        [582, 194,  878,  114,  0,    536,  764,  388, 730,  776, 1118, 400,  514,  708,  1050, 674,  1244],
-        [274, 502,  502,  650,  536,  0,    228,  308, 194,  240, 582,  776,  662,  628,  514,  1050, 708 ],
-        [502, 730,  274,  878,  764,  228,  0,    536, 194,  468, 354,  1004, 890,  856,  514,  1278, 480 ],
-        [194, 354,  810,  502,  388,  308,  536,  0,   342,  388, 730,  468,  354,  320,  662,  742,  856 ],
-        [308, 696,  468,  844,  730,  194,  194,  342, 0,    274, 388,  810,  696,  662,  320,  1084, 514 ],
-        [194, 742,  742,  890,  776,  240,  468,  388, 274,  0,   342,  536,  422,  388,  274,  810,  468 ],
-        [536, 1084, 400,  1232, 1118, 582,  354,  730, 388,  342, 0,    878,  764,  730,  388,  1152, 354 ],
-        [502, 594,  1278, 514,  400,  776,  1004, 468, 810,  536, 878,  0,    114,  308,  650,  274,  844 ],
-        [388, 480,  1164, 628,  514,  662,  890,  354, 696,  422, 764,  114,  0,    194,  536,  388,  730 ],
-        [354, 674,  1130, 822,  708,  628,  856,  320, 662,  388, 730,  308,  194,  0,    342,  422,  536 ],
-        [468, 1016, 788,  1164, 1050, 514,  514,  662, 320,  274, 388,  650,  536,  342,  0,    764,  194 ],
-        [776, 868,  1552, 560,  674,  1050, 1278, 742, 1084, 810, 1152, 274,  388,  422,  764,  0,    798 ],
-        [662, 1210, 754,  1358, 1244, 708,  480,  856, 514,  468, 354,  844,  730,  536,  194,  798,  0   ],
+        [0,   548,  776, 696,  582,  274,  502,  194, 308,  194, 536,  502,  388,  354,  468,  776,  662 ],
+        [548, 0,    684, 308,  194,  502,  730,  354, 696,  742, 1084, 594,  480,  674,  1016, 868,  1210],
+        [776, 684,  0,   992,  878,  502,  274,  810, 468,  742, 400,  1278, 1164, 1130, 788,  1552, 754 ],
+        [696, 308,  992, 0,    114,  650,  878,  502, 844,  890, 1232, 514,  628,  822,  1164, 560,  1358],
+        [582, 194,  878, 114,  0,    536,  764,  388, 730,  776, 1118, 400,  514,  708,  1050, 674,  1244],
+        [274, 502,  502, 650,  536,  0,    228,  308, 194,  240, 582,  776,  662,  628,  514,  1050, 708 ],
+        [502, 730,  274, 878,  764,  228,  0,    536, 194,  468, 354,  1004, 890,  856,  514,  1278, 480 ],
+        [194, 354,  810, 502,  388,  308,  536,  0,   342,  388, 730,  468,  354,  320,  662,  742,  856 ],
+        [308, 696,  468, 844,  730,  194,  194,  342, 0,    274, 388,  810,  696,  662,  320,  1084, 514 ],
+        [194, 742,  742, 890,  776,  240,  468,  388, 274,  0,   342,  536,  422,  388,  274,  810,  468 ],
+        [536, 1084, 400, 1232, 1118, 582,  354,  730, 388,  342, 0,    878,  764,  730,  388,  1152, 354 ],
+        [502, 594,  1278, 514, 400,  776,  1004, 468, 810,  536, 878,  0,    114,  308,  650,  274,  844 ],
+        [388, 480,  1164, 628, 514,  662,  890,  354, 696,  422, 764,  114,  0,    194,  536,  388,  730 ],
+        [354, 674,  1130, 822, 708,  628,  856,  320, 662,  388, 730,  308,  194,  0,    342,  422,  536 ],
+        [468, 1016, 788, 1164, 1050, 514,  514,  662, 320,  274, 388,  650,  536,  342,  0,    764,  194 ],
+        [776, 868,  1552, 560, 674,  1050, 1278, 742, 1084, 810, 1152, 274,  388,  422,  764,  0,    798 ],
+        [662, 1210, 754, 1358, 1244, 708,  480,  856, 514,  468, 354,  844,  730,  536,  194,  798,  0   ],
     ]
     data['demands'] = [0, 1, 1, 2, 4, 2, 4, 8, 8, 1, 2, 1, 2, 4, 4, 8, 8]
     data['vehicle_capacity'] = 15
     return data
 
-def diagonals_must_be_zero(model : cp_model.CpModel, matrix, n_vehicles, n_locations):
-    for v in range(n_vehicles):
-        for i in range(n_locations):
-            model.Add(matrix[v][i][i] == 0)
 
-def enforce_matrix_usage(model : cp_model.CpModel, matrix, matrix_usage, n_vehicles, n_locations):
-    for v in range(n_vehicles):
-        origin_row_sum    = sum(matrix[v][0][i] for i in range(n_locations))
-        origin_column_sum = sum(matrix[v][i][0] for i in range(n_locations))
+def print_solution(data, manager, routing, solution):
+    """Prints solution on console."""
+    print(f'Objective: {solution.ObjectiveValue()}')
+    total_distance = 0
+    total_load = 0
+    for vehicle_id in range(len(data['distance_matrix'])):
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) ->'.format(node_index, route_load)
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id)
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index), route_load)
+        plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
 
-        model.Add(origin_row_sum    == matrix_usage[v])
-        model.Add(origin_column_sum == matrix_usage[v])
+        if route_load == 0:
+            continue
 
-        matrix_sum = sum(matrix[v][i][j] for i in range(n_locations) for j in range(n_locations))
-
-        model.Add(matrix_sum <= matrix_usage[v] * n_locations)
-
-def all_destinations_must_be_visited(model : cp_model.CpModel, matrix, n_vehicles, n_locations):
-    for i in range(n_locations):
-        for j in range(n_locations):
-            s = sum(matrix[v][i][j] for v in range(n_vehicles))
-            model.Add(s <= 1) # This one is more of a sanity check
-    
-    for i in range(1, n_locations):
-        s = sum(matrix[v][i][j] for v in range(n_vehicles) for j in range(n_locations))
-        model.Add(s == 1)
-    
-    for j in range(1, n_locations):
-        s = sum(matrix[v][i][j] for v in range(n_vehicles) for i in range(n_locations))
-        model.Add(s == 1)
-
-def paths_must_make_sense(model : cp_model.CpModel, matrix, n_vehicles, n_locations):
-    for v in range(n_vehicles):
-        for i in range(n_locations):
-            i_row_sum = sum(matrix[v][i][j] for j in range(n_locations))
-            i_col_sum = sum(matrix[v][j][i] for j in range(n_locations))
-            model.Add(i_col_sum == i_row_sum)
-
-def minimize_travel_time(model : cp_model.CpModel, matrix, n_vehicles, n_locations, distance_matrix):
-    min_sum = 0
-    for v in range(n_vehicles):
-        for i in range(n_locations):
-            for j in range(n_locations):
-                min_sum += matrix[v][i][j] * distance_matrix[i][j]
-    
-    model.Minimize(min_sum)
-
-def enforce_maximum_demands(model : cp_model.CpModel, matrix, n_vehicles, n_locations, demands, vehicle_capacity):
-    for v in range(n_vehicles):
-        demand_sum = 0
-        for i in range(n_locations):
-            for j in range(n_locations):
-                demand_sum += matrix[v][i][j] * demands[i]
-        model.Add(demand_sum <= vehicle_capacity)
+        print(plan_output)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
 
 
-def solve(data_model):
-    model = cp_model.CpModel()
+def main():
+    """Solve the CVRP problem."""
+    # Instantiate the data problem.
+    data = create_data_model()
 
-    n_locations = len(data_model["distance_matrix"])
-    n_vehicles = 5 # data_model['vehicle_capacity']
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), len(data['distance_matrix']), 0)
 
-    matrix = [] # matrix[vehicle][from][to]
-    for v in range(n_vehicles):
-        square = []
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
 
-        for i in range(n_locations):
-            square.append([model.NewBoolVar(f"{v}-{i}-{j}") for j in range(n_locations)])
-        matrix.append(square)
-    
-    matrix_usage = [model.NewBoolVar(f"u-{l}") for l in range(n_vehicles)]
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
 
-    diagonals_must_be_zero(model, matrix, n_vehicles, n_locations)
-    enforce_matrix_usage  (model, matrix, matrix_usage, n_vehicles, n_locations)
-    all_destinations_must_be_visited(model, matrix, n_vehicles, n_locations)
-    paths_must_make_sense(model, matrix, n_vehicles, n_locations)
-    minimize_travel_time(model, matrix, n_vehicles, n_locations, data_model["distance_matrix"])
-    enforce_maximum_demands(model, matrix, n_vehicles, n_locations, data_model["demands"], data_model['vehicle_capacity'])
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
 
-    solver = cp_model.CpSolver()
-    status = solver.Solve(model)
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
-        print("Objective:", solver.ObjectiveValue())
-        print([solver.Value(i) for i in matrix_usage])
 
-        for v in range(n_vehicles):
-            if solver.Value(matrix_usage[v]) == 0:
-                continue
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
 
-            print(f"V{v}")
-            for i in range(n_locations):
-                print(" ".join(['1' if solver.Value(matrix[v][i][j]) else '.' for j in range(n_locations)]))
+    demand_callback_index = routing.RegisterUnaryTransitCallback(demand_callback)
+    routing.AddDimension(
+        demand_callback_index, 
+        0,  # null capacity slack
+        data['vehicle_capacity'], # vehicle maximum capacities
+        True, # start cumul to zero
+        "Capacity"
+    )
 
-if __name__ == "__main__":
-    solve(create_data_model())
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+    search_parameters.local_search_metaheuristic = (routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.time_limit.FromSeconds(1)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(data, manager, routing, solution)
+
+
+if __name__ == '__main__':
+    main()
