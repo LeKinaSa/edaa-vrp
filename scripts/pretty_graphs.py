5f86ed812bfa262f3ae9071ae0da96c291c0b46a
Daniel Monteiro <daniel.amaro.monteiro@gmail.com>
2022-06-20 02:39:25 +0100

    Number of vehicles barplot

--- scripts/pretty_graphs.py
+++ scripts/pretty_graphs.py
@@ -1,6 +1,6 @@
 import pandas as pd, seaborn as sns, matplotlib.pyplot as plt
 
-sns.set(font_scale=1.5)
+sns.set(font_scale=1.5) # Adgust this if necessary
 
 CVRP_FILES = [
     ("data/cvrp-benchmarks/greedy_analysis.csv",        "Greedy"),
@@ -32,8 +32,11 @@ if __name__ == "__main__":
     print(data)
     print(data[data["method"] == "Constraint Programming"])
 
-    sns.lineplot(data=data, x="num_deliveries", y="time_us", hue="method", lw=2).set(title="Method time performance (μs) (lower is better)")
+    sns.lineplot(data=data, x="num_deliveries", y="time_us", hue="method", lw=4).set(title="Time performance (μs) (lower is better)")
     plt.show()
 
-    sns.barplot(data=data, x="num_deliveries", y="solution_length", hue="method").set(title="Method solution quality (lower is better)")
+    sns.barplot(data=data, x="num_deliveries", y="solution_length", hue="method").set(title="Solution quality (lower is better)")
+    plt.show()
+
+    sns.barplot(data=data, x="num_deliveries", y="num_vehicles", hue="method").set(title="Number of trucks (lower is better)")
     plt.show()
