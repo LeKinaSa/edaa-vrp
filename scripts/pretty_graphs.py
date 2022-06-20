9ae98c0f4be6f2e933a76a47baf4eb47d57bc620
Clara Martins <clara.alves.martins@hotmail.com>
2022-06-20 07:29:24 +0100

    Printing the graphs to files

--- scripts/pretty_graphs.py
+++ scripts/pretty_graphs.py
@@ -1,6 +1,6 @@
 import pandas as pd, seaborn as sns, matplotlib.pyplot as plt
 
-sns.set(font_scale=1.5) # Adgust this if necessary
+sns.set(font_scale=1.5) # Adjust this if necessary
 
 CVRP_FILES = [
     ("data/cvrp-benchmarks/greedy_analysis.csv",        "Greedy"),
@@ -30,26 +30,37 @@ def get_data() -> pd.DataFrame:
 if __name__ == "__main__":
     data = get_data()
 
+    figsize = (22, 11)
+    i = 1
+    plt.figure(figsize=figsize)
     sns.lineplot(data=data, x="num_deliveries", y="time_us", hue="method", lw=4).set(title="Time performance (μs) (lower is better)")
-    plt.show()
+    plt.savefig(f"data/pretty_graphs/graph{i}.png")
 
+    i += 1
+    plt.figure(figsize=figsize)
     sns.barplot(data=data, x="num_deliveries", y="solution_length", hue="method").set(title="Solution quality (lower is better)")
-    plt.show()
+    plt.savefig(f"data/pretty_graphs/graph{i}.png")
 
+    i += 1
+    plt.figure(figsize=figsize)
     sns.barplot(data=data, x="num_deliveries", y="num_vehicles", hue="method").set(title="Number of trucks (lower is better)")
-    plt.show()
+    plt.savefig(f"data/pretty_graphs/graph{i}.png")
 
+    i += 1
+    plt.figure(figsize=figsize)
     sns.barplot(data=data, x="num_deliveries", y="average_load", hue="method").set(title="Average load (higher is better)")
     plt.legend(loc='lower right')
-    plt.show()
+    plt.savefig(f"data/pretty_graphs/graph{i}.png")
 
 
     instances = list(data[data["method"] == "Constraint Programming"]["name"])
 
     for instance in instances:
+        i += 1
         instance_data = data[data["name"] == instance]
+        plt.figure(figsize=figsize)
         sns.scatterplot(data=instance_data, x="time_us", y="solution_length", hue="method", s=300).\
             set(title=f"Time vs. solution length (instance {instance})", ylim=(0, None), xscale="log")
             
         plt.legend(loc='lower right')
-        plt.show()
+        plt.savefig(f"data/pretty_graphs/graph{i}.png")
